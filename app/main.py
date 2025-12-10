0aba8a89a39187296727ae4b184f44e23a6927f8
pksingh <pksingh69313@gmail.com>
2025-12-10 23:36:01 +0530

    fixed broken css

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+# from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 
 app = FastAPI(title="Task API")
+# app.add_middleware(ProxyHeadersMiddleware, trusted_hosts=[*])
 
 # STATIC FILES
 app.mount("/static", StaticFiles(directory="static"), name="static")
