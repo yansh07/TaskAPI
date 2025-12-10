bb122bbc5c9d5d3e4eeeb1b0e810b641cc568210
pksingh <pksingh69313@gmail.com>
2025-12-10 22:42:21 +0530

    fixed tailwind warnings

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+export default {
+  experimental: {
+    optimizeUniversalDefaults: false,
+  },
+  rules: {
+    // disable canonical class suggestions
+    'suggestCanonicalClasses': false,
+  }
+}
